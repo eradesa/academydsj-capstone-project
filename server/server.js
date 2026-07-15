4f52c4110716f58debe5bf7f0ece145f7ce61307
eradesa <eranga.de.saram@gmail>
2026-07-15 08:11:10 +0530

    Fix: include devDeps in build, root redirects to landing page

--- server/server.js
+++ server/server.js
@@ -13,12 +13,12 @@ app.use(express.json());
 
 app.use("/landing", express.static(path.join(__dirname, "..", "landing")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "client", "dist")));
-  app.get("/app", (req, res) => res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html")));
-}
-
 app.get("/", (req, res) => res.redirect("/landing/index.html"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/app", express.static(path.join(__dirname, "..", "client", "dist")));
+  app.get("/app/*", (req, res) => res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html")));
+}
 
 mongoose
   .connect(process.env.MONGODB_URI)
